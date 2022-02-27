79a18affb5668701ca6eeb6f73cca1228d04d242
L N V YASWANTH <lnvyeswanth1575@gmail.com>
2022-02-27 21:41:01 +0530

    UPDATED

---
+++ JAVA CA1/src/Q1.java
@@ -0,0 +1,7 @@
+public class Q1 {
+    public static void main(String arg[]) {
+        int A=10,B=0; if(A<B & A/B==10)
+            System.out.print("Wow"); else
+            System.out.print("Great:");
+    }
+}
