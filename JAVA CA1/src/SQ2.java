052912e72cd891e382f29f6b0d497e08d8e8c29f
L N V YASWANTH <lnvyeswanth1575@gmail.com>
2022-03-02 16:22:20 +0530

    UPDATED

---
+++ JAVA CA1/src/SQ2.java
@@ -0,0 +1,7 @@
+public class SQ2 {
+    public static void main(String arg[]) {
+        //singed rigth shift opperator
+        //100 is shifted rig
+        int x=100,y; System.out.print(x>>2);
+    }
+}
---
+++ JAVA CA1/src/SQ5.java
@@ -0,0 +1,2 @@
+public class SQ5 {
+}
