610cd8694b4730fc3b6ab7b6fa8a755dca8b643f
L N V YASWANTH <lnvyeswanth1575@gmail.com>
2022-03-11 21:30:40 +0530

    UPDATED

---
+++ src/com/company/TRY.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class TRY {
+    public static void main(String[] args){
+        int[]  x = {120, 200, 016};
+        for(int i = 0; i < x.length; i++){
+            System.out.print(x[i] +" ");
+        }
+    }
+}
