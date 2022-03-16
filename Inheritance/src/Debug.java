7d48f717e01c9831ab67eb41b05df14a43775956
L N V YASWANTH <lnvyeswanth1575@gmail.com>
2022-03-16 17:08:16 +0530

    UPDATED

---
+++ Inheritance/src/Debug.java
@@ -0,0 +1,36 @@
+public class Debug {
+    class School {
+        String name;
+        int numStudents;
+
+        public School(String name, int numStudents) {
+            this.name = name;
+            this.numStudents = numStudents;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+
+        public int getNumStudents() {
+            return this.numStudents;
+        }
+    }
+
+
+    class University extends School {
+        private final String type = "University";
+        private boolean isPublic;
+
+        public University(String name, int numStudents, boolean isPublic) {
+            super(name,numStudents);
+            this.name = name;
+            this.numStudents = numStudents;
+            this.isPublic = isPublic;
+        }
+
+        public boolean getIsPublic() {
+            return this.isPublic;
+        }
+    }
+}
