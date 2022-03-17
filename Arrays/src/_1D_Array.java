80914d1bafad9ac92ccb30fa4be466a3929f2881
L N V YASWANTH <lnvyeswanth1575@gmail.com>
2022-03-17 20:03:16 +0530

    UPDATED

---
+++ Arrays/src/_1D_Array.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class _1D_Array {
+    public static void main(String args[]){
+        ArrayList array= new ArrayList();
+        array.add(20);
+        array.add(15);
+        array.add(22);
+        System.out.println(array.size());
+        array.remove(0);
+        System.out.println(array);
+        System.out.println(array.contains(20));
+    }
+}
