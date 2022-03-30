d6bb8a5e704803d4cfce35c6335d6cd800c345f0
L N V YASWANTH <66690784+yaswanth-github@users.noreply.github.com>
2022-03-30 10:10:04 +0530

    Create simple_interest.sh

---
+++ simple_interest.sh
@@ -0,0 +1,13 @@
+SIMPLE INTEREST
+
+Simple interest is interest calculated on the principal portion of a loan or the original contribution to a savings account. Simple interest does not compound, meaning that an account holder will only gain interest on the principal, and a borrower will never have to pay interest on interest already accrued.
+
+You can calculate simple interest in a savings account by multiplying the account balance by the interest rate by the time period the money is in the account.
+
+Here's the simple interest formula: Interest = P x R x N.
+
+P = Principal amount (the beginning balance).
+
+R = Interest rate (usually per year, expressed as a decimal).
+
+N = Number of time periods (generally one-year time periods).
