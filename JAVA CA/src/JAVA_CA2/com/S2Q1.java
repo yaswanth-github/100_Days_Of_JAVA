956c81a6377155f956bb691488100dcc2b882cce
L N V YASWANTH <lnvyeswanth1575@gmail.com>
2022-04-06 10:32:10 +0530

    UPDATED

--- JAVA CA/src/JAVA_CA2/com/S2Q1.java
+++ JAVA CA/src/JAVA_CA2/com/S2Q1.java
@@ -1,4 +1,30 @@
 package JAVA_CA2.com;
-
+import java.util.*;
 public class S2Q1 {
+        public static void main(String args[]) {
+            Scanner s = new Scanner(System.in);
+            ArrayList<Integer> list = new ArrayList<>();
+            System.out.println("Enter Array Length ::");
+            int n = s.nextInt();
+            for (int i = 0; i < n; i++)
+            {
+                list.add(s.nextInt());
+            }
+            reverseArrayList(list);
+            for (int i = 0; i < list.size(); i++)
+            {
+                System.out.print(list.get(i) + " ");
+            }
+        }
+
+        // Method to reverse the ArrayList
+        static void reverseArrayList(ArrayList<Integer> list) {
+            int leftIndex = 0, rightIndex = list.size() - 1;
+            int temp;
+            while (leftIndex < rightIndex) {
+                temp = list.get(leftIndex);
+                list.set(leftIndex++, list.get(rightIndex));
+                list.set(rightIndex--, temp);
+            }
+        }
 }
--- JAVA CA/src/JAVA_CA2/com/S3Q2.java
+++ JAVA CA/src/JAVA_CA2/com/S3Q2.java
@@ -1,4 +1,32 @@
 package JAVA_CA2.com;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class S3Q2 {
+        static void isAnagram(String str1, String str2) {
+            String s1 = str1.replaceAll("\\s", "");
+            String s2 = str2.replaceAll("\\s", "");
+            boolean status = true;
+            if (s1.length() != s2.length()) {
+                status = false;
+            } else {
+                char[] ArrayS1 = s1.toLowerCase().toCharArray();
+                char[] ArrayS2 = s2.toLowerCase().toCharArray();
+                Arrays.sort(ArrayS1);
+                Arrays.sort(ArrayS2);
+                status = Arrays.equals(ArrayS1, ArrayS2);
+            }
+            if (status) {
+                System.out.println(s1 + " and " + s2 + " are anagrams");
+            } else {
+                System.out.println(s1 + " and " + s2 + " are not anagrams");
+            }
+        }
+
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            String a=input.nextLine();
+            String b=input.nextLine();
+            isAnagram(a,b);
+        }
 }
