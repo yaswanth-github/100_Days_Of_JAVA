def432ef19dd694373f19507edc0a23f8f9c47bf
L N V YASWANTH <lnvyeswanth1575@gmail.com>
2022-04-08 10:04:21 +0530

    UPDATED

---
+++ JAVA CA/src/JAVA_CA2/com/JS1Q3.java
@@ -0,0 +1,12 @@
+package JAVA_CA2.com;
+
+public class JS1Q3 {
+    public static void main(String[] args) {
+        String Data = "Hi, Bye,Go,Low,High,Show,No";
+        String[] numArray = new String[0];
+//        System.out.println(Data.toString(numArray));
+//        for (String str : numArray) {
+//            System.out.println(str);
+//        }
+    }
+}
--- JAVA CA/src/JAVA_CA2/com/S3Q1.java
+++ JAVA CA/src/JAVA_CA2/com/S3Q1.java
@@ -3,7 +3,7 @@ package JAVA_CA2.com;
 import java.util.Scanner;
 
 public class S3Q1 {
-    public static void main(String[] args) {
+    public static void main(String args[]) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter The full Name ::");
         String name=input.nextLine();
@@ -22,10 +22,12 @@ public class S3Q1 {
         }
         String str2 = "";
         for (int j = 0; j < str1.length(); j++) {
-            if (j == 0)
+            if (j == 0) {
                 str2 = str2 + Character.toUpperCase(str1.charAt(0));
-            else
+            }
+            else {
                 str2 = str2 + Character.toLowerCase(str1.charAt(j));
+            }
         }
         System.out.println(str2);
     }
