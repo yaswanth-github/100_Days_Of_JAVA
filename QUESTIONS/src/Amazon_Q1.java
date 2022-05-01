b69c153ea73bf2ddc905a7ffea850ab8ae9ae500
L N V YASWANTH <lnvyeswanth1575@gmail.com>
2022-05-01 20:18:15 +0530

    UPDATED

---
+++ QUESTIONS/src/Amazon_Q1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Amazon_Q1 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int n=input.nextInt();
+        if(n%2==0){
+
+        }
+        else{
+
+        }
+    }
+}
