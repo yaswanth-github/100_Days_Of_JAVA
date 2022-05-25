e8a5d4453b4738bb6edc4f0fe3bd6b8f35ae6ebc
L N V YASWANTH <lnvyeswanth1575@gmail.com>
2022-05-25 11:10:15 +0530

    UPDATED

--- src/com/company/TRY.java
+++ src/com/company/TRY.java
@@ -1,10 +1,8 @@
 package com.company;
 
 public class TRY {
-    public static void main(String[] args){
-        int[]  x = {120, 200, 016};
-        for(int i = 0; i < x.length; i++){
-            System.out.print(x[i] +" ");
-        }
+    public static void main(String args[]) {
+        String a= "jaswanth";
+        System.out.println("Hello World"+a);
     }
 }
