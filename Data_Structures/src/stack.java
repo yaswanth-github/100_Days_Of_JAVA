d1fb42a3eff50ac439cca8e85866a964da52432f
L N V YASWANTH <66690784+yaswanth-github@users.noreply.github.com>
2022-08-05 15:37:47 +0530

    DS

---
+++ Data_Structures/src/stack.java
@@ -0,0 +1,13 @@
+public class stack {
+    public static void main(String arg[]){
+        while(true){
+            System.out.println("1.push");
+            System.out.println("2.pop");
+            System.out.println("3.peek");
+            System.out.println("4.traverse");
+            System.out.println("5.quit");
+            System.out.println("Enter Your Choice ::");
+
+        }
+    }
+}
