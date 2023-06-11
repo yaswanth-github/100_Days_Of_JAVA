565c5196bee247e328d446f1bf163334280af214
L N V YASWANTH <66690784+yaswanth-github@users.noreply.github.com>
2023-06-11 18:59:58 +0530

    Update on Jun

---
+++ TCS/test.java
@@ -0,0 +1,27 @@
+class Test {
+
+    public static void main(String[] args) {
+
+
+        display("Welcome");
+
+    }
+
+    private static void display(String s) {
+
+// TODO Auto-generated method stub
+
+        System.out.println("String");
+
+    }
+
+
+    private static void display(int s) {
+
+// TODO Auto-generated method stub
+
+        System.out.println("int");
+
+    }
+
+}
